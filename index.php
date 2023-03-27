c3e2d9212fd5996b8c741230fbbfdc7c466f6d6c
georgelongland <george@mosaic-app.com>
2023-03-27 11:48:07 +0100

    css styling added to the form, validation messages and error messages

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 // import functions
 
 require_once "functions/makeApiCall.php";
-
 require 'vendor/autoload.php';
 
 $errors = array("firstName" => "", "title" => "");
@@ -13,6 +12,7 @@ $title = '';
 if (isset($_POST["submit"])) {
     $title = $_POST["title"];
     $firstName = $_POST["firstName"];
+    $errors = array('title' => '', 'firstName' => '');
 
     //form validation title field
     if ($_POST["title"] == "") {
@@ -27,13 +27,13 @@ if (isset($_POST["submit"])) {
     }
 
     // change title to all lowercase for inclusion in the api call
-    $title = strtolower($title);
+    $titleForApiCall = strtolower($title);
 
     if ( array_filter($errors)) {
         //echo errors in form
     } else {
         try {
-            makeApiCall($title, $firstName);
+            makeApiCall($titleForApiCall, $firstName);
             $messages["success"] = "The API call was made successfully";
         } catch (Exception $e) {
             $messages["error"] = "There was an error making the API call";
@@ -46,33 +46,38 @@ if (isset($_POST["submit"])) {
 <html lang="eng">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title></title>
 </head>
 <body>
-<form action="index.php" method="post">
-    <label for="title">Title</label>
-    <br>
-    <select id="title" name="title" class="form-control">
-        <option <?php if ($title == "") echo "selected"; ?> value="">Select title</option>
-        <option <?php if ($title == "Mr") echo "selected"; ?> value="Mr">Mr</option>
-        <option <?php if ($title == "Mrs") echo "selected"; ?> value="Mrs">Mrs</option>
-        <option <?php if ($title == "Miss") echo "selected"; ?> value="Miss">Miss</option>
-        <option <?php if ($title == "Ms") echo "selected"; ?> value="Ms">Ms</option>
-        <option <?php if ($title == "Dr") echo "selected"; ?> value="Dr">Dr</option>
-    </select>
-    <div><?php echo $errors["title"]?></div>
-    <label for="firstName">First Name</label>
-    <br>
-    <input type="text" id="firstName" name="firstName" class="form-control" value="<?php echo htmlspecialchars($firstName)?>">
-    <div><?php echo $errors["firstName"]?></div>
-    <br>
-    <input type="submit" name="submit" value="submit">
-</form>
-<div>
-    <div><?php echo $messages["success"]; ?></div>
-    <div><?php echo $messages["error"]; ?></div>
-    <p></p>
+<div class="row">
+    <div class="col-4"></div>
+    <div class="col-4">
+        <form action="index.php" method="post">
+            <label for="title">Title</label>
+            <br>
+            <select id="title" name="title" class="form-control">
+                <option <?php if ($title == "") echo "selected"; ?> value="">Select title</option>
+                <option <?php if ($title == "Mr") echo "selected"; ?> value="Mr">Mr</option>
+                <option <?php if ($title == "Mrs") echo "selected"; ?> value="Mrs">Mrs</option>
+                <option <?php if ($title == "Miss") echo "selected"; ?> value="Miss">Miss</option>
+                <option <?php if ($title == "Ms") echo "selected"; ?> value="Ms">Ms</option>
+                <option <?php if ($title == "Dr") echo "selected"; ?> value="Dr">Dr</option>
+            </select>
+            <div class="red-text"><?php echo $errors["title"]?></div>
+            <label for="firstName">First Name</label>
+            <br>
+            <input type="text" id="firstName" name="firstName" class="form-control" value="<?php echo htmlspecialchars($firstName)?>">
+            <div class="red-text"><?php echo $errors["firstName"]?></div>
+            <br>
+            <input type="submit" name="submit" value="submit">
+        </form>
+        <div>
+            <div class="green-text"><?php echo $messages["success"]; ?></div>
+            <div class="red-text"><?php echo $messages["error"]; ?></div>
+        </div>
+    </div>
 </div>
 </body>
 </html>
